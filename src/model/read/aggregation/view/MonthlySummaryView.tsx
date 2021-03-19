cc4e5afc0763f4ff0bdfbe1b72845b9bdcc4df7c
fukuchiharuki <fukuchi.haruki@gmail.com>
2021-03-19 23:13:54 +0900

    feat: MonthlySummaryView

---
+++ src/model/read/aggregation/view/MonthlySummaryView.tsx
@@ -0,0 +1,29 @@
+import MonthlySummary from '../MonthlySummary';
+import Amount from 'ui/Amount';
+import Mount from 'ui/Mount';
+
+type Props = {
+  children: MonthlySummary
+};
+
+function MonthlySummaryView(props: Props) {
+  const monthlySummary = props.children;
+  return (
+    <dl className="monthly-summary">
+      <dt>収入</dt>
+      <dd><Amount>{monthlySummary.収入()}</Amount></dd>
+      <dt>支出</dt>
+      <dd><Amount>{monthlySummary.特別費を除いた支出()}</Amount></dd>
+      <Mount if={monthlySummary.特別費()}>
+        <dd>(+ 特別費: <Amount>{monthlySummary.特別費()}</Amount>)</dd>
+      </Mount>
+      <dt>差引</dt>
+      <dd><Amount>{monthlySummary.特別費を含めた差引()}</Amount></dd>
+      <Mount if={monthlySummary.特別費()}>
+        <dd>(特別費込: <Amount>{monthlySummary.特別費を含めた差引()}</Amount>)</dd>
+      </Mount>
+    </dl>
+  );
+}
+
+export default MonthlySummaryView;
