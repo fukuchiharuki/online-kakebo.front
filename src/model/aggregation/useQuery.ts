0f00ef0274d168d3c6045f3e8ad56ec0b904beee
fukuchiharuki <fukuchi.haruki@gmail.com>
2021-03-28 10:07:24 +0900

    fix: useCallbackの依存

--- src/model/aggregation/useQuery.ts
+++ src/model/aggregation/useQuery.ts
@@ -8,8 +8,8 @@ function useQuery(dataSource: DataSource) {
   const url = dataSource.aggregation();
   const [state, dispatch] = useModel();
   const callback: Callback = {
-    preProcess: useCallback(() => dispatch(fetchAction()), []),
-    postProcess: useCallback((json) => dispatch(fetchedAction(json)), [])
+    preProcess: useCallback(() => dispatch(fetchAction()), [dispatch]),
+    postProcess: useCallback((json) => dispatch(fetchedAction(json)), [dispatch])
   }
   useFetch(url, callback);
   return state;
