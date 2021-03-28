30c3002d5b538a1bbb33286b0b6f5945288793e6
fukuchiharuki <fukuchi.haruki@gmail.com>
2021-03-28 10:03:53 +0900

    feat: fetchのロギング

--- src/infrastructure/useFetch.ts
+++ src/infrastructure/useFetch.ts
@@ -8,12 +8,15 @@ export default function useFetch(
   useEffect(() => {
     (async () => {
       preProcess && preProcess();
+      console.log(`get ${url}`);
       const response = await fetch(url);
       if (response.ok) {
         const json = await response.json();
         postProcess && postProcess(json);
+        console.log(`ok`);
       } else {
         errorHandler && errorHandler();
+        console.log(`error`);
       }
     })();
   }, [url, preProcess, postProcess, errorHandler]);
