a940a0bc1145fdc547bac77347bbbd9cb2ee9dad
fukuchiharuki <fukuchi.haruki@gmail.com>
2021-04-04 06:00:46 +0900

    fix: 特別費を含めない差引の計算に誤り

--- src/model/aggregation/view/MonthlySummaryView.tsx
+++ src/model/aggregation/view/MonthlySummaryView.tsx
@@ -15,12 +15,14 @@ function MonthlySummaryView(props: Props) {
       <dt>支出</dt>
       <dd><Amount>{monthlySummary.特別費を除いた支出()}</Amount></dd>
       <Mount if={monthlySummary.特別費()}>
-        <dd>(+ 特別費: <Amount>{monthlySummary.特別費()}</Amount>)</dd>
+        <dd>+ 特別費: <Amount>{monthlySummary.特別費()}</Amount></dd>
+        <dd>= 計: <Amount>{monthlySummary.支出()}</Amount></dd>
       </Mount>
       <dt>差引</dt>
-      <dd><Amount>{monthlySummary.特別費を含めた差引()}</Amount></dd>
+      <dd><Amount>{monthlySummary.特別費を含めない差引()}</Amount></dd>
       <Mount if={monthlySummary.特別費()}>
-        <dd>(特別費込: <Amount>{monthlySummary.特別費を含めた差引()}</Amount>)</dd>
+        <dd>- 特別費: <Amount>{monthlySummary.特別費()}</Amount></dd>
+        <dd>= 計: <Amount>{monthlySummary.特別費を含めた差引()}</Amount></dd>
       </Mount>
     </dl>
   );
