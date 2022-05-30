8b0b017129e233dc7bac0379edcbfc2fbf9e5ade
fukuchiharuki <fukuchi.haruki@gmail.com>
2022-05-30 23:07:15 +0900

    fix: 特別費の表示

--- src/components/aggregation/MonthlySummaryView.tsx
+++ src/components/aggregation/MonthlySummaryView.tsx
@@ -15,14 +15,12 @@ function MonthlySummaryView(props: Props) {
       <dt>支出</dt>
       <dd><Amount>{monthlySummary.特別費を除いた支出()}</Amount></dd>
       <Mount if={monthlySummary.特別費()}>
-        <dd>+ 特別費: <Amount>{monthlySummary.特別費()}</Amount></dd>
-        <dd>= 計: <Amount>{monthlySummary.支出()}</Amount></dd>
+        <dd>+ <Amount>{monthlySummary.特別費()}</Amount>(特別費) = <Amount>{monthlySummary.支出()}</Amount></dd>
       </Mount>
       <dt>差引</dt>
       <dd><Amount>{monthlySummary.特別費を含めない差引()}</Amount></dd>
       <Mount if={monthlySummary.特別費()}>
-        <dd>- 特別費: <Amount>{monthlySummary.特別費()}</Amount></dd>
-        <dd>= 計: <Amount>{monthlySummary.特別費を含めた差引()}</Amount></dd>
+        <dd>- <Amount>{monthlySummary.特別費()}</Amount>(特別費) = <Amount>{monthlySummary.特別費を含めた差引()}</Amount></dd>
       </Mount>
     </dl>
   );
